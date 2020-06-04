ac54b844255f0b684750a3ff9081d1648e8b0550
hahmadh <ah8902106@gmail.com>
2020-06-04 11:17:52 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <h1>
-        Ahmad Hannan Masood Baig. MughAL
+        Ahmad Hannan Masood Baig. MughAL Sahib
       </h1>
     </div>
   );
